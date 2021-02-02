b61ae7dd344d929f78d03ac5acc81aa3fdedb295
kysaeed <kysaeed@gmail.com>
2021-02-02 12:35:10 +0900

    テストコード削除

--- ship-ob/resources/js/bootstrap.js
+++ ship-ob/resources/js/bootstrap.js
@@ -75,23 +75,6 @@ if (userAgentName === 'firefox') {
     global.isFirefox = true;
 }
 
-
-if((userAgent.indexOf('msie') > -1) || (userAgent.indexOf('trident') > -1)) {
-    console.log('Internet Explorerをお使いですね');
-} else if(userAgent.indexOf('edge') != -1) {
-    console.log('Edgeをお使いですね');
-} else if(userAgent.indexOf('chrome') != -1) {
-    console.log('Google Chromeをお使いですね');
-} else if(userAgent.indexOf('safari') != -1) {
-    console.log('Safariをお使いですね');
-} else if(userAgent.indexOf('firefox') != -1) {
-    console.log('FireFoxをお使いですね');
-} else if(userAgent.indexOf('opera') != -1) {
-    console.log('Operaをお使いですね');
-} else {
-    console.log('そんなブラウザは知らん');
-}
-
 window.uaList = {};
 
 
